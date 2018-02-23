53307a22b8ca9e1f564c45694d7665767d6d9b5e
kawal2266 <yatbkawal2266@gmail.com>
2018-02-23 22:45:09 +0530

    Made Chat Application

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+let port = 5000 || process.env.PORT;
+let users = [];
+let connections = [];
+
+app.use(express.static('public'));
+
+io.on('connect',function(socket){
+    connections.push(socket);
+    console.log(`Connected : ${connections.length} students are present.`);
+
+    socket.on('disconnect',function(data){
+        users.splice(users.indexOf(socket.username),1)
+        updateUsernames();
+        connections.splice(connections.indexOf(socket),1);
+        console.log(`Disconnected : ${connections.length} students present now`)
+    })
+
+    socket.on('send message',function(data){
+        io.sockets.emit('new message',{msg : data, user : socket.username});
+    })
+
+    socket.on('new user',function(data ,callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    })
+
+    function updateUsernames(){
+        io.sockets.emit('get users',users);
+    }
+})
+
+http.listen( port ,function()
+{
+    console.log(`Server is running ${port}!!`)
+})
